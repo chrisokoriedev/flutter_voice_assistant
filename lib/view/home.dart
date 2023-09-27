e72562dddce1ca4b50cd94c7d8cd9858a4958b90
christian-iuil <okoriec01@gmail.com>
2023-09-27 08:06:19 +0100

    update

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_voice_assistant/pallete.dart';
 
+import 'reuseable_wdget.dart';
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
@@ -66,59 +68,10 @@ class Homepage extends StatelessWidget {
           )
         ]),
       ),
-      floatingActionButton: FloatingActionButton.small(onPressed: (){},child: const Icon(Icons.mic),),
-    );
-  }
-}
-
-class ResuableCompenet extends StatelessWidget {
-  final String title;
-  final String des;
-  final VoidCallback? press;
-  final Color color;
-  const ResuableCompenet({
-    super.key,
-    required this.textStyle,
-    required this.title,
-    required this.des,
-    this.press,
-    required this.color,
-  });
-
-  final TextTheme textStyle;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-        decoration: BoxDecoration(
-            color: color, borderRadius: BorderRadius.circular(15)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(title,
-                style: textStyle.bodyLarge!
-                    .copyWith(fontWeight: FontWeight.bold, height: 2)),
-            Text(des,
-                style: textStyle.bodyMedium!
-                    .copyWith(fontWeight: FontWeight.w500)),
-          ],
-        ),
+      floatingActionButton: FloatingActionButton.small(
+        onPressed: () {},
+        child: const Icon(Icons.mic),
       ),
     );
-  }
-}
-
-class Gap extends StatelessWidget {
-  const Gap({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(height: 20);
   }
 }
